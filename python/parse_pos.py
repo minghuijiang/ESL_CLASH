81e11655bf0c4b8bdae4264d9162a544363f8489
Ming <mjian002@odu.edu>
2015-03-25 01:38:23 +0800

    fix python shell

--- python/parse_pos.py
+++ python/parse_pos.py
@@ -10,7 +10,7 @@ def parse_text(inputText):
     out = list()
     for para in inputText.splitlines():
         print para
-        print para.encode('utf-8','ignore')
+        print para.encode('ascii','ignore')
         print para.encode('utf-8','replace')
         pList = list()
         sents = nltk.sent_tokenize(para.decode('utf-8'))
