d76a4ddaf0c2db33d2ddd4496827fc45f20b5548
minghuijiang32@gmail.com <36453201@Git>
2015-04-04 09:18:30 +0800

    fix login page.

--- routes/routes.js
+++ routes/routes.js
@@ -24,8 +24,8 @@ module.exports = function(app, passport) {
 
     // process the login form
     app.post('/login', passport.authenticate('local-login', {
-            successRedirect : '/profile', // redirect to the secure profile section
-            failureRedirect : '/login', // redirect back to the signup page if there is an error
+            successRedirect : '/dev/profile', // redirect to the secure profile section
+            failureRedirect : '/dev/login', // redirect back to the signup page if there is an error
             failureFlash : true // allow flash messages
         }),
         function(req, res) {
