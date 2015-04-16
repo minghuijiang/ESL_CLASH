0b4e7d8c998e4281473853ae81c1e45add59c8a9
MingJiang <minghuijiang32@gmail.com>
2015-04-16 23:46:24 +0800

    message display

--- public/js/event.js
+++ public/js/event.js
@@ -22,12 +22,12 @@ function bindLoad(button, panel, url,callback){
             console.log(data);
             if(data.error){
                 showError(data.error);
-                return ;
-            }
-            for(var i=0;i<data.data.length;i++){
-                var option = document.createElement('option');
-                option.text =callback(data.data[i]);
-                select.append(option);
+            }else{
+                for(var i=0;i<data.data.length;i++){
+                    var option = document.createElement('option');
+                    option.text =callback(data.data[i]);
+                    select.append(option);
+                }
             }
         })
     })
