fa39d365dac1dc86f0f2986279fa29b50967c240
Ming <minghuijiang32@gmail.com>
2016-01-13 13:42:32 -0500

    test

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-//test
+//test2
 
 
 // uncomment after placing your favicon in /public
