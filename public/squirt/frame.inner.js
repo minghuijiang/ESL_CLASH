9e90499d535cd40ee80c97f79245cd1cce40aad1
Ming <minghuijiang32@gmail.com>
2016-01-17 22:05:13 -0500

    log key event.

--- public/squirt/frame.inner.js
+++ public/squirt/frame.inner.js
@@ -1112,12 +1112,12 @@ var keyHandlers = {
         27: evt.dispatch.bind(null, 'squirt.close', {}, null),
         38: evt.dispatch.bind(null, 'squirt.wpm.adjust', {value: 10}, null),
         40: evt.dispatch.bind(null, 'squirt.wpm.adjust', {value: -10}, null),
-        37: sq.playing?evt.dispatch.bind(null, 'squirt.rewind.start', {}, null):evt.dispatch.bind(null, 'squirt.previous', {}, null),
-        39: sq.playing?evt.dispatch.bind(null, 'squirt.ff.start', {}, null):evt.dispatch.bind(null, 'squirt.next', {}, null)
+        37: sq.playing?evt.dispatch.bind(null, 'squirt.rewind.start', {}, null):null,
+        39: sq.playing?evt.dispatch.bind(null, 'squirt.ff.start', {}, null):null
     },
     keyup: {
-        37: evt.dispatch.bind(null, 'squirt.rewind.stop', {}, null),
-        39: evt.dispatch.bind(null, 'squirt.ff.stop', {}, null),
+        37: sq.playing?evt.dispatch.bind(null, 'squirt.rewind.stop', {}, null):evt.dispatch.bind(null, 'squirt.previous', {}, null),
+        39: sq.playing?evt.dispatch.bind(null, 'squirt.ff.stop', {}, null):evt.dispatch.bind(null, 'squirt.next', {}, null),
         83: evt.dispatch.bind(null, 'squirt.toggleSettings', {}, null)
     }
 };
